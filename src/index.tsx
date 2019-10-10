ae1a0d047d5e131a2fae8ed7b0b7a7d07f178c08
osdevisnot <osdevisnot@gmail.com>
2019-10-10 12:32:16 -0700

    linting fixes

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import { readFileSync, existsSync } from 'fs';
+import { existsSync, readFileSync } from 'fs';
 import { join } from 'path';
 import { name, version } from '../package.json';
 
@@ -6,28 +6,30 @@ const log = message => console.log(`${name}@${version} - ${message}`);
 
 const isDefined = value => (typeof value !== 'undefined' && value !== null ? true : false);
 
-export default (options = {}) => {
+export default (options: any = {}) => {
 	const env = isDefined(process.env.NODE_ENV) && process.env.NODE_ENV !== '' ? process.env.NODE_ENV : 'development';
 
-	const wd = isDefined(options) && isDefined(options['dir']) ? options['dir'] : process.cwd();
+	const wd = isDefined(options) && isDefined(options.dir) ? options.dir : process.cwd();
 
 	[`.env.${env}`, `.env`].forEach(relative => {
 		const absolute = join(wd, relative);
 
 		if (existsSync(absolute)) {
-			let content = readFileSync(absolute, 'utf-8').toString();
+			const content = readFileSync(absolute, 'utf-8').toString();
 
 			content.split(/\n|\r|\r\n/).forEach(line => {
 				if (isDefined(line) && line !== '') {
 					const keyVal = line.match(/^\s*([\w.-]+)\s*=\s*(.*)?\s*$/);
 
 					if (isDefined(keyVal)) {
+						// tslint:disable-next-line:prefer-const
 						let [, key, value] = keyVal;
 
 						while (value.indexOf('process.env.') !== -1) {
 							value = value.replace(/\$\{process.env.(.*?)\}/, (_, b) => {
-								if (isDefined(process.env[b])) return process.env[b];
-								else {
+								if (isDefined(process.env[b])) {
+									return process.env[b];
+								} else {
 									log(
 										`Attempt to use 'undefined' value in interpolations. key: ${key}, interpolation: ${b}, value: ${process.env[b]}`,
 									);
